a8ebe20fa8c9fd6a1f393236a88374105e4bb0e3
RRRRRRain233 <72107015+RRRRRRain233@users.noreply.github.com>
2023-05-05 02:36:39 -0700

    function compilation

---
+++ data/log_reg.py
@@ -0,0 +1,51 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import precision_recall_curve, roc_curve, auc
+from sklearn.metrics import classification_report
+from matplotlib import pyplot
+from matplotlib import pylab
+import pandas as pd
+import numpy as np
+
+def training():
+    df = pd.read_csv("training.csv")
+    df = df.drop(columns='Unnamed: 0', axis=1)
+    df = df.drop(columns='County', axis=1)
+    df = df.drop(columns='Date', axis=1)
+
+    accuracy = 0
+
+    for i in range(0,10):
+      df1 = df.iloc[0 : i*300]
+      df2 = df.iloc[i*300 : (i+1)*300]
+      df3 = df.iloc[(i+1)*300 :]
+
+      train_df = pd.concat([df1, df3])
+      test_df = df2
+      
+      X_train = train_df[['Temperature', 'Precipitation']]
+      Y_train = train_df[['Is_Burned']]
+      Y_train = Y_train.to_numpy()
+      Y_train = Y_train.reshape(-1)
+      X_test = test_df[['Temperature', 'Precipitation']]
+      Y_test = test_df[['Is_Burned']]
+      Y_test = Y_test.to_numpy()
+      Y_test = Y_test.reshape(-1)
+
+      average = 0
+      testNum = 10
+
+      lr = LogisticRegression(max_iter=3000)
+      lr.fit(X_train, Y_train)
+      Y_pred = lr.predict(X_test)
+      p = 0
+      for i in range(len(Y_test)):
+          if Y_test[i] == Y_pred[i]:
+              p += 1
+      p /= len(Y_test)
+      accuracy += p
+    print(accuracy/10)
+
+
+
+training()
--- data/log_reg_v2.py
+++ data/log_reg_v2.py
@@ -12,27 +12,36 @@ def log_reg_v2():
     df = df.drop(columns='Unnamed: 0', axis=1)
     df = df.drop(columns='County', axis=1)
     df = df.drop(columns='Date', axis=1)
-    train_df = df.iloc[0:3400]
-    test_df = df.iloc[3400:]
-    
-    X_train = train_df[['Temperature', 'Prev 1 Temp', 'Prev 2 Temp', 'Precipitation', 'Prev 1 Prec', 'Prev 2 Prec']]
-    Y_train = train_df[['Is_Burned']]
-    Y_train = Y_train.to_numpy()
-    Y_train = Y_train.reshape(-1)
-    X_test = test_df[['Temperature', 'Prev 1 Temp', 'Prev 2 Temp', 'Precipitation', 'Prev 1 Prec', 'Prev 2 Prec']]
-    Y_test = test_df[['Is_Burned']]
-    Y_test = Y_test.to_numpy()
-    Y_test = Y_test.reshape(-1)
 
-    lr = LogisticRegression()
-    lr.fit(X_train, Y_train)
-    Y_pred = lr.predict(X_test)
-    p = 0
-    for i in range(len(Y_test)):
-        if Y_test[i] == Y_pred[i]:
-            p += 1
-    p /= len(Y_test)
-    print(p)
+    accuracy = 0
+
+    for i in range(0,10):
+        df1 = df.iloc[0 : i*300]
+        df2 = df.iloc[i*300 : (i+1)*300]
+        df3 = df.iloc[(i+1)*300 :]
+        train_df = pd.concat([df1, df3])
+        test_df = df2
+        
+        X_train = train_df[['Temperature', 'Prev 1 Temp', 'Precipitation', 'Prev 1 Prec']]
+        Y_train = train_df[['Is_Burned']]
+        Y_train = Y_train.to_numpy()
+        Y_train = Y_train.reshape(-1)
+        X_test = test_df[['Temperature', 'Prev 1 Temp', 'Precipitation', 'Prev 1 Prec']]
+        Y_test = test_df[['Is_Burned']]
+        Y_test = Y_test.to_numpy()
+        Y_test = Y_test.reshape(-1)
+
+        lr = LogisticRegression(max_iter=3000)
+        lr.fit(X_train, Y_train)
+        Y_pred = lr.predict(X_test)
+        p = 0
+        for i in range(len(Y_test)):
+            if Y_test[i] == Y_pred[i]:
+                p += 1
+        p /= len(Y_test)
+        accuracy += p
+    
+    print(accuracy/10)
 
 
 
